13d51302b4dbd14cc87556119cd24a8a8417856e
m4rtxn <teranxr@gmail.com>
2025-04-23 15:51:42 -0600

    Edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({menuOpen,setMenuOpen}) => {
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white"> 
-                        Mi CV
+                       <span></span>
                         </a>
                
 
