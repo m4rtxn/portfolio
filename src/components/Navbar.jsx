4054b0b8f613a7d0e0f50bb9b0730f6f4d8a3778
m4rtxn <teranxr@gmail.com>
2025-04-23 15:20:13 -0600

    Edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,7 @@ export const Navbar = ({menuOpen,setMenuOpen}) => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.0)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white"> 
-                        Martin
-                        <span className="text-blue-500">Codes</span></a>
-               
+              
 
                     <div 
                     className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
